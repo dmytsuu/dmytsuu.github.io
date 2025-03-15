2c1534e9432dbc73ef522c754f8e455516606db4
dmytsuu <grayeyed16@gmail.com>
2025-03-15 05:30:57 +0200

    Initial

---
+++ sw.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = 'healthpick-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/phone-login.html',
+    '/signup.html',
+    '/authorization.html',
+    '/product-selection.html',
+    '/medbox-selection.html',
+    '/medication-retrieval.html',
+    'https://cdn.tailwindcss.com',
+    'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css',
+    'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then(response => {
+                        // Don't cache if not a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response
+                        const responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    });
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+}); 
